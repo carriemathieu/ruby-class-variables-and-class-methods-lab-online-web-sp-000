4a4d3a743adee9af9d792427823014ed7af0cd05
Carrie Mathieu <carrieburks@gmail.com>
2020-08-13 03:53:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,7 @@ class Song
   
   def self.genre_count
     genre_count = {} # returns hash in which keys are names of each genre 
+    
     @@genres.each do |genre| # iterates through @@genres
       # if genre exists in list up to this point, add to existing count
       if genre_count[genre] 
@@ -43,6 +44,7 @@ class Song
   
   def self.artist_count
     artist_count = {} # returns hash in which keys are names of each genre 
+    
     @@artists.each do |artist| # iterates through @@artists
     # if genre exists in list up to this point, add to existing count
       if artist_count[artist] 
