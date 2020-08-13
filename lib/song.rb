9c53c6f091e4ab029c0f9c8d162a9d70944b2979
Carrie Mathieu <carrieburks@gmail.com>
2020-08-13 03:45:29 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = [] 
+  @@artists = []
+ 
+  def initialize(name, artist, genre)
+    @@count +=1
+    
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@artists << @artist
+    @@genres << @genre 
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def artists
+   #ontrol for duplicates when you code your .artists class method, not when you add artists to the original @@artists array. We will want to know how many songs each have been assigned to each artist.
+  end
+  
+ def genre
+    #Control for duplicates when you code your .genres class method, not when you add genres to the original @@genres array. We will want to know how many songs of each genre have been created.
+  end
+  
+  def genre_count
+    #returns hash in which keys are names of each genre 
+    #each genre name key should point to value that is the # of songs for that genre
+    #will need to iterate over @@genres & populate hash w/ key/value pairs. check for duplicates, if so, increment value by one, else create new key value/pair
+  end
+  
+  def artist_count
+  end
+end
